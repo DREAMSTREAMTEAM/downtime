1982c20751d09ea1ca3e15f4c65c75dc52eb7464
andino <andy.andino93@gmail.com>
2018-05-14 13:27:35 -0700

    (init) webpack hot middleware with load in dev only

---
+++ server/middlewares/webpackMiddleware.js
@@ -0,0 +1,15 @@
+/* istanbul ignore next */
+export default function mountWebpackMiddleware(server) {
+  // disable for global requires since airbnb doesn't like them being in a block
+  if (process.env.NODE_ENV === 'development') {
+    /* eslint-disable */
+    const config = require('../../webpack/webpack.config');
+    const compiler = require('webpack')(config);
+    server.use(require('webpack-dev-middleware')(compiler, {
+      noInfo: true,
+      publicPath: config.output.publicPath,
+    }));
+    server.use(require('webpack-hot-middleware')(compiler));
+    /* eslint-enable */
+  }
+}
