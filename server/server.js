63c56cd1b4f956f5bf5ee8baffc6871bcab15b3a
andino <andy.andino93@gmail.com>
2018-05-14 13:26:53 -0700

    (init) server with middleware for helment, cors, logging, and custom middlewares

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import Cors from '@koa/cors';
+import BodyParser from 'koa-bodyparser';
+import Helmet from 'koa-helmet';
+import respond from 'koa-respond';
+import serveStatic from 'koa-static';
+
+import webpackHotMiddleware from './middlewares/webpackMiddleware';
+import logger from './middlewares/logger';
+
+const app = new Koa();
+
+app.use(Helmet());
+app.use(Cors());
+app.use(BodyParser());
+app.use(respond());
+webpackHotMiddleware(app);
+logger(app);
+app.use(serveStatic('../client', { defer: true }));
+
+export default app;
