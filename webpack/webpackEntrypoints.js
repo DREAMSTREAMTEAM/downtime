e3e16e7fe2f03677c4aea141db91da97baf66d38
andino <andy.andino93@gmail.com>
2018-05-14 15:18:54 -0700

    (refactor) remove unneeded entries

--- webpack/webpackEntrypoints.js
+++ webpack/webpackEntrypoints.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
-const SRC_DIR = path.join(__dirname, '/client');
+const SRC_DIR = path.join(__dirname, '../client');
 
 module.exports = () => {
   const entry = {
     production: [`${SRC_DIR}/index.jsx`],
-    development: ['webpack-hot-middleware/client', `${SRC_DIR}/index.jsx`],
+    development: [`${SRC_DIR}/index.jsx`],
   };
 
   return entry[process.env.NODE_ENV];
