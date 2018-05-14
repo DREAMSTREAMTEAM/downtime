6329c975eee012b72ae6361bf88ee2bbe5dd9f92
andino <andy.andino93@gmail.com>
2018-05-14 15:18:04 -0700

    (feature) add middle to search static assets

--- server/server.js
+++ server/server.js
@@ -1,25 +1,24 @@
 import Koa from 'koa';
-import Router from 'koa-router';
 import Cors from '@koa/cors';
 import BodyParser from 'koa-bodyparser';
 import Helmet from 'koa-helmet';
 import respond from 'koa-respond';
 import serveStatic from 'koa-static';
+import path from 'path';
 
 import searchRoutes from './routes/search';
-import webpackHotMiddleware from './middlewares/webpackMiddleware';
+import webpackMiddleware from './middlewares/webpackMiddleware';
 import logger from './middlewares/logger';
 
 const app = new Koa();
-const router = new Router();
 
 app.use(Helmet());
 app.use(Cors());
 app.use(BodyParser());
 app.use(respond());
-webpackHotMiddleware(app);
+webpackMiddleware(app);
 logger(app);
-app.use(serveStatic('../client', { defer: true }));
+app.use(serveStatic(path.join(__dirname, '../client')));
 app.use(searchRoutes.routes());
 
 export default app;
